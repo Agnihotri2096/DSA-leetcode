3a6b75152e7b39ad86ef9afa99f347dc15888db2
Akshat Agnihotri <136256863+Agnihotri2096@users.noreply.github.com>
2026-07-02 15:28:09 +0530

    Time: 3 ms (56.43%), Space: 19.7 MB (97.86%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,4 +4,12 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
+        # this is a simple approach but two pointer approach is accepted here
+        '''s[:] = s[::-1]'''
+        #two pointer approach
+        left = 0
+        right = len(s) - 1
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left +=1
+            right-=1
