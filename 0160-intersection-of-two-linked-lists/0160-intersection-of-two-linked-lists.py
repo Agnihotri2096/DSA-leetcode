e72c2dfa91027ea4eb08b92e88ac453bdd38c63f
Akshat Agnihotri <136256863+Agnihotri2096@users.noreply.github.com>
2026-07-09 12:47:54 +0530

    Enhance code clarity with additional comments
    Added comments to clarify the purpose of the code.

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -10,12 +10,14 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
+         # added all elements of listA in set
         hash = set()
         curr = headA
         while curr:
             hash.add(curr)
             curr = curr.next
         currB = headB
+         #compare traversing listB with listA and where value becomes same return that
         while currB:
             if currB in hash:
                 return currB
